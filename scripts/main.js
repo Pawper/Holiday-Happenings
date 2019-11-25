1f388bc115ef5ff86f8a4cfe819afc2a8c0b3db3
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2019-11-24 21:28:07 -0800

    haven't been using git haha

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,23 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Copy anchor link to heading
+function copyLink(clicked_element)
+{
+  var pageURL = document.location.href.match(/(^[^#]*)/)[0];  
+  var headingID = clicked_element.parentElement.parentElement.id;
+  var copiedLink = pageURL + '#' + headingID;
+  var clipboardStaging = document.createElement('textarea');
+  clipboardStaging.value = copiedLink;
+  clipboardStaging.setAttribute('readonly', '');
+  clipboardStaging.style = {position: 'absolute', left: '-9999px'};
+  document.body.appendChild(clipboardStaging);
+  clipboardStaging.select();
+  document.execCommand('copy');
+  document.body.removeChild(clipboardStaging);
+  var toast = document.getElementById("toast");
+  toast.innerHTML = "Copied link " + copiedLink;
+  toast.className = "show";
+  setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 2900);
+}
